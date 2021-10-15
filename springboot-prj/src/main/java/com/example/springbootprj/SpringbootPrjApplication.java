07eeeac51691525ff078b90ef5112fd50cb1a8b6
justmb <50409706+justmb@users.noreply.github.com>
2021-10-15 09:53:10 +0700

    Add files via upload

---
+++ springboot-prj/src/main/java/com/example/springbootprj/SpringbootPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPrjApplication.class, args);
+	}
+
+}
---
+++ springboot-prj/src/test/java/com/example/springbootprj/SpringbootPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
